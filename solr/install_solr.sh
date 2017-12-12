feb6a383ea3c227011a1ba3f71942eb269d3bc18
arnikz <a.kuzniar@esciencecenter.nl>
2017-12-12 17:27:30 +0100

    Fix solr installation

--- solr/install_solr.sh
+++ solr/install_solr.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 
 # 1. Install Solr version (6.2.1)
-wget -nc http://archive.apache.org/dist/lucene/solr/6.2.1/solr-6.2.1.tgz -O solr-6.2.1.tgz
-tar -xvf solr-6.2.1.tgz --directory ./
-ln -sf solr-6.2.1/ solr
+URL=http://archive.apache.org/dist/lucene/solr/6.2.1/solr-6.2.1.tgz
+wget -qO- $URL | tar xvz --strip=1 -C solr
 
 # 2. Start Solr server incl. cores
-solr/bin/solr start -p 8983 -s core/data
+cd $(dirname $0) && bin/solr start -p 8983 -s core/data
 
