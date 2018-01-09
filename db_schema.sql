d6d37d0c1e0deaf2037d18875773f9b1da6d8603
arnikz <a.kuzniar@esciencecenter.nl>
2018-01-09 16:24:40 +0100

    Yet another round #47

--- db_schema.sql
+++ db_schema.sql
@@ -1,69 +1,54 @@
 CREATE TABLE ARTICLE (
-  pmc_id  TEXT PRIMARY KEY NOT NULL,
-  title   TEXT
+  pmc_id  NUMERIC NOT NULL,
+  title   TEXT,
+  PRIMARY KEY(pmc_id)
 );
 
 CREATE TABLE TRAIT_TABLE (
-  tab_id  TEXT PRIMARY KEY NOT NULL,
-  n_cols  INTEGER NOT NULL,
-  n_rows  INTEGER NOT NULL,
-  pmc_id  TEXT NOT NULL,
+  tab_id  NUMERIC NOT NULL,
+  pmc_id  NUMERIC NOT NULL,
+  PRIMARY KEY(tab_id),
   FOREIGN KEY(pmc_id) REFERENCES ARTICLE(pmc_id)
 );
 
 CREATE TABLE ABBREVIATION (
   abbrev    TEXT NOT NULL,
   expansion TEXT NOT NULL,
-  pmc_id    TEXT NOT NULL,
+  pmc_id    NUMERIC NOT NULL,
   FOREIGN KEY(pmc_id) REFERENCES ARTICLE(pmc_id)
 );
 
 CREATE TABLE COLUMN_ENTRY (
-  col_id  TEXT PRIMARY KEY NOT NULL,
+  col_id  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  tab_id  NUMERIC NOT NULL,
   header  TEXT,
   type    TEXT,
   annot   TEXT,
-  tab_id  TEXT,
   FOREIGN KEY(tab_id) REFERENCES TRAIT_TABLE(tab_id)
 );
 
 CREATE TABLE CELL_ENTRY (
-  cel_id  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  row_id  INTEGER,
+  row_id  NUMERIC NOT NULL,
+  col_id  NUMERIC NOT NULL,
   value   TEXT,
   type    TEXT,
-  col_id  TEXT,
+  PRIMARY KEY (row_id, col_id),
   FOREIGN KEY(col_id) REFERENCES COLUMN_ENTRY(col_id)
 );
 
-CREATE TABLE TRAIT (
-  trait_id          INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  trait_name        TEXT,
-  trait_value_pair  TEXT,
-  trait_prop_pair   TEXT,
-  other_trait_pair  TEXT,
-  pmc_id            TEXT,
-  tab_id            TEXT,
-  row_id            INTEGER,
-  FOREIGN KEY(pmc_id) REFERENCES ARTICLE(pmc_id),
-  FOREIGN KEY(tab_id) REFERENCES TRAIT_TABLE(tab_id)
-);
-
 CREATE TABLE QTL (
-  qtl_id            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  pmc_id            NUMERIC NOT NULL,
+  tab_id            NUMERIC NOT NULL,
+  row_id            NUMERIC NOT NULL,
   trait_in_article  TEXT,
   trait_in_onto     TEXT,
-  trait_uri         TEXT, --> trait_id?
+  trait_uri         TEXT,
   chromosome        TEXT,
   marker            TEXT,
-  marker_uri        TEXT, --> marker_id?
+  marker_uri        TEXT,
   gene              TEXT,
-  gene_uri          TEXT, --> gene_id?
-  --snpAssociated     TEXT, --> marker
-  --snpUri            TEXT, --> marker_uri
-  pmc_id            TEXT,
-  tab_id            TEXT,
-  row_id            INTEGER,
+  gene_uri          TEXT,
+  PRIMARY KEY(pmc_id, tab_id, row_id),
   FOREIGN KEY(pmc_id) REFERENCES ARTICLE(pmc_id),
   FOREIGN KEY(tab_id) REFERENCES TRAIT_TABLE(tab_id)
 );
