363ab0e708623f43787311d1b70cef8c0f3c8848
gurnoor1990 <gurnoor1990@gmail.com>
2018-11-14 14:34:59 +0100

    Fixes #72

--- src/main/java/solrAnnotator/AbbrevtoSynonyms.java
+++ src/main/java/solrAnnotator/AbbrevtoSynonyms.java
@@ -1,7 +1,6 @@
 package solrAnnotator;
 
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,7 +8,6 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Properties;
 import java.util.Set;
 
 import qtm.Article;
@@ -26,64 +24,88 @@ public class AbbrevtoSynonyms {
 	public static void abbrevToSolrSynonyms(Article articles[])
 			throws FileNotFoundException, UnsupportedEncodingException,
 			IOException {
-		String coreTraitDescSynonmsFile = Configs.getPropertyQTM("solrCorePath") + "/"
-				+ Configs.getPropertyQTM("coreTraitDescriptors") + "/conf/synonyms.txt";
+		String coreTraitDescSynonmsFile = Configs.getPropertyQTM("solrCorePath")
+				+ "/" + Configs.getPropertyQTM("coreTraitDescriptors")
+				+ "/conf/synonyms.txt";
 
-		HashMap<String, String> oldAbbrev = new HashMap<String, String>();
-		Properties properties = new Properties();
+		// HashMap<String, String> oldAbbrev = new HashMap<String, String>();
+		// Properties properties = new Properties();
 
-		properties.load(new FileInputStream(coreTraitDescSynonmsFile));
+		// properties.load(new FileInputStream(coreTraitDescSynonmsFile));
 
-		for (String key : properties.stringPropertyNames()) {
-			oldAbbrev.put(key.toString(), properties.get(key).toString());
-		}
+		// System.out.println(properties.toString());
+
+		// for (String key : properties.stringPropertyNames()) {
+		// oldAbbrev.put(key.toString(), properties.get(key).toString());
+		// }
+
+
 
 		FileWriter fw = new FileWriter(coreTraitDescSynonmsFile, true);
 		BufferedWriter bw = new BufferedWriter(fw);
 		PrintWriter writer = new PrintWriter(bw);
 
-		for (int i = 0; i < articles.length; i++) {
-			if (articles[i] != null) {
-				HashMap<String, String> abbrevMap = articles[i]
-						.getAbbreviations();
-				if (abbrevMap.isEmpty()) {
-					break;
-				} else {
-					Set<String> totalkeys = abbrevMap.keySet();
-					Iterator<String> keys = totalkeys.iterator();
-					while (keys.hasNext()) {
-						String shortform = keys.next();
-						if (oldAbbrev.containsKey(shortform)) {
-							break;
-						} else {
-							writer.println(shortform + " => "
-									+ abbrevMap.get(shortform));
-							System.out.println(shortform + " => "
-									+ abbrevMap.get(shortform));
-						}
-					}
 
+		HashMap<String, String> allAbbreviation = new HashMap<String, String>();
+
+
+
+		try{
+		for (int i = 0; i < articles.length; i++) {
+			HashMap<String, String> abbrevin1article = articles[i].getAbbreviations();
+			if (abbrevin1article.isEmpty()) {
+				continue;
+			} else {
+				Set<String> totalkeys = abbrevin1article.keySet();
+				Iterator<String> keys = totalkeys.iterator();
+				while (keys.hasNext()) {
+					String shortform = keys.next();
+					if (allAbbreviation.containsKey(shortform)) {
+						allAbbreviation.put(shortform, allAbbreviation.get(shortform) + "," + abbrevin1article.get(shortform));
+					} else {
+						allAbbreviation.put(shortform, abbrevin1article.get(shortform));
+					}
 				}
+
 			}
 		}
-		writer.close();
+		}
+		catch(Exception e){
+			System.out.println("No Abbreviations, detected or copied");
+		}
 
-		String coreTraitValuesDir = Configs.getPropertyQTM("solrCorePath")
-				+ Configs.getPropertyQTM("coreTraitValues") + "/conf/";
-		String coreTraitPropertiesDir = Configs.getPropertyQTM("solrCorePath")
-				+ Configs.getPropertyQTM("coreTraitProperties") + "/conf/";
-
-		try {
-			Process p1 = Runtime.getRuntime()
-					.exec("cp " + coreTraitDescSynonmsFile + " " + coreTraitValuesDir);
-			p1.waitFor();
-			Process p2 = Runtime.getRuntime()
-					.exec("cp " + coreTraitDescSynonmsFile + " " + coreTraitPropertiesDir);
-			p2.waitFor();
-
-		} catch (Exception e) {
+		Set<String> totalkeys = allAbbreviation.keySet();
+		Iterator<String> keys = totalkeys.iterator();
+		while (keys.hasNext()) {
+			String shortform = keys.next();
+			writer.println(shortform + " => "
+					+ allAbbreviation.get(shortform));
+			//System.out.println(shortform + " => "
+			//		+ allAbbreviation.get(shortform));
 
 		}
+
+
+		writer.close();
+
+		// String coreTraitValuesDir = Configs.getPropertyQTM("solrCorePath")
+		// + Configs.getPropertyQTM("coreTraitValues") + "/conf/";
+		// String coreTraitPropertiesDir =
+		// Configs.getPropertyQTM("solrCorePath")
+		// + Configs.getPropertyQTM("coreTraitProperties") + "/conf/";
+		//
+		// try {
+		// Process p1 = Runtime.getRuntime()
+		// .exec("cp " + coreTraitDescSynonmsFile + " " + coreTraitValuesDir);
+		// p1.waitFor();
+		// Process p2 = Runtime.getRuntime()
+		// .exec("cp " + coreTraitDescSynonmsFile + " " +
+		// coreTraitPropertiesDir);
+		// p2.waitFor();
+		//
+		// } catch (Exception e) {
+		//
+		// }
 
 	}
 
